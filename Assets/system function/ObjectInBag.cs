f85348aff46b36f118cfa378c438299a0336e354
小羊宝正 <15712326888@139.com>
2023-11-24 21:50:16 +0800

    粗犷的改了bug

--- Assets/system function/ObjectInBag.cs
+++ Assets/system function/ObjectInBag.cs
@@ -34,20 +34,28 @@ public class ObjectInBag : MonoBehaviour, IBeginDragHandler, IEndDragHandler, ID
     public void OnEndDrag(PointerEventData eventData)
     {
         GetComponent<RectTransform>().sizeDelta = new Vector2(orginalWidth, orginalHeight);
-
-        if (eventData.pointerCurrentRaycast.gameObject.tag == "BagCell")
+        if (eventData.pointerCurrentRaycast.gameObject.tag != null)
         {
-            this.transform.SetParent(eventData.pointerCurrentRaycast.gameObject.transform);
-            this.transform.position = this.transform.parent.position;
-            GetComponent<CanvasGroup>().blocksRaycasts = true;
-        }
-        else if (eventData.pointerCurrentRaycast.gameObject.tag == "BagObject")
-        {
-            this.transform.SetParent(eventData.pointerCurrentRaycast.gameObject.transform.parent);
-            this.transform.position = this.transform.parent.position;
-            eventData.pointerCurrentRaycast.gameObject.transform.parent = orginalParentTrans;
-            eventData.pointerCurrentRaycast.gameObject.transform.position = orginalParentTrans.position;
-            GetComponent<CanvasGroup>().blocksRaycasts = true;
+            if (eventData.pointerCurrentRaycast.gameObject.tag == "BagCell")
+            {
+                this.transform.SetParent(eventData.pointerCurrentRaycast.gameObject.transform);
+                this.transform.position = this.transform.parent.position;
+                GetComponent<CanvasGroup>().blocksRaycasts = true;
+            }
+            else if (eventData.pointerCurrentRaycast.gameObject.tag == "BagObject")
+            {
+                this.transform.SetParent(eventData.pointerCurrentRaycast.gameObject.transform.parent);
+                this.transform.position = this.transform.parent.position;
+                eventData.pointerCurrentRaycast.gameObject.transform.parent = orginalParentTrans;
+                eventData.pointerCurrentRaycast.gameObject.transform.position = orginalParentTrans.position;
+                GetComponent<CanvasGroup>().blocksRaycasts = true;
+            }
+            else
+            {
+                this.transform.SetParent(orginalParentTrans);
+                this.transform.position = orginalParentTrans.position;
+                GetComponent<CanvasGroup>().blocksRaycasts = true;
+            }
         }
         else
         {
@@ -55,5 +63,6 @@ public class ObjectInBag : MonoBehaviour, IBeginDragHandler, IEndDragHandler, ID
             this.transform.position = orginalParentTrans.position;
             GetComponent<CanvasGroup>().blocksRaycasts = true;
         }
+
     }
 }
